ebf3222502195abf79be0aa66a11362e0f56eb23
shelliaronov <shelliaronov24@gmail.com>
2025-11-12 16:42:59 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,14 +1,16 @@
 def move(my_list, direction):
     if 1 not in my_list:
-      print("Error: The number 1 is not in the list.")
-      return my_list
-    
-    index_of_one = my_list.index(1)
-    
+        print("Error: The number 1 is not in the list.")
+        return my_list 
+
+    index_of_one = my_list.index(1) 
+
     if direction == 'right' and index_of_one < len(my_list) - 1:
         my_list[index_of_one] = 0
         my_list[index_of_one + 1] = 1
     elif direction == 'left' and index_of_one > 0:
         my_list[index_of_one] = 0
         my_list[index_of_one - 1] = 1
+        
+
     return my_list
